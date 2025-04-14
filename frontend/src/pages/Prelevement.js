652ff58e0f0592cfabec6301c8cddc981434489f
LaurentVerto <99186365+LaurentVerto@users.noreply.github.com>
2025-04-14 15:30:35 +0200

    nettoyage prelevements

--- frontend/src/pages/Prelevement.js
+++ frontend/src/pages/Prelevement.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
+import useContratServices from "../services/ContratsServices";
+import ListeContratsInf15 from "../components/contratsFilter/ListeContratsInf15"
+import ListeContratsSup15 from "../components/contratsFilter/ListeContratsSup15";
 
 function Prelevement() {
 
-    const [mesContrats, setMesContrats] = useState([])
+    const {mesContrats, setMesContrats} = useContratServices ();
+
     const contentRef = useRef(null);
     const [scrollUp, setScrollUp] = useState(false);
     const [scrollDown, setScrollDown] = useState(false);
-
 
     useEffect(() => {
         const content = contentRef.current;
@@ -33,39 +36,18 @@ function Prelevement() {
         }
     }, []);  // Dépendances vides => s'exécute une seule fois après le premier rendu
 
-    // Ajout d'un effet supplémentaire pour forcer la mise à jour après le premier rendu
     useEffect(() => {
-        const timeout = setTimeout(() => {
-            const content = contentRef.current;
-            if (content) {
-                // Vérifie de nouveau si le contenu est plus grand que l'espace visible
-                const isScrollable = content.scrollHeight > content.clientHeight;
-                setScrollDown(isScrollable);
-            }
-        }, 50); // Timeout à 0 pour qu'il se produise après le premier rendu complet
-
-        return () => clearTimeout(timeout);
-    }, []);
-
-
-    const trieContrats = mesContrats.sort((a, b) => new Date(a.datePrlvt) - new Date(b.datePrlvt))
-
-    useEffect(() => {
-        const dataLocal = localStorage.getItem("mesContrats");
-        if (dataLocal) {
-            setMesContrats(JSON.parse(dataLocal))
-        }
-    }, [])
-
-    const total = mesContrats ? mesContrats.reduce((total, contrat) => total + parseFloat(contrat.prix), 0).toFixed(2) : 0;
-    //si il y a des contrats tu fait un calcul : on creer donc 2 variable : total initialiser a 0 et contrat qui sera float de contrat.prix
-
-
-    const listeAffichage = mesContrats
-        .filter((contrat) => new Date(contrat.datePrlvt).getDate() > 15)  // Filtrer les contrats
-        .reduce((acc, contrat) => acc + (parseFloat(contrat.prix) || 0), 0)  // S'assurer que chaque prix est un nombre valide
-        .toFixed(2);
-
+            const timeout = setTimeout(() => {
+                const content = contentRef.current;
+                if (content) {
+                    // Vérifie de nouveau si le contenu est plus grand que l'espace visible
+                    const isScrollable = content.scrollHeight > content.clientHeight;
+                    setScrollDown(isScrollable);
+                }
+            }, 50); // Timeout à 0 pour qu'il se produise après le premier rendu complet
+    
+            return () => clearTimeout(timeout);
+    }, []);// Ajout d'un effet supplémentaire pour forcer la mise à jour après le premier rendu
 
     return (
         <div className="main-content">
@@ -80,110 +62,10 @@ function Prelevement() {
 
             <div ref={contentRef} className="overflow-y-scroll h-[calc(100vh-220px)]">
 
-
-
                 {mesContrats.length === 0 && <p className="text-center mt-5">Aucun contrat enregistré</p>}
 
-
-                {mesContrats.some(contrat => {
-                    const day = new Date(contrat.datePrlvt).getDate();
-                    return day <= 15 && contrat.statusAbo === true;
-                }) && (
-                        <ul className="flex justify-center w-[100%] flex-col items-center relative mt-3 subpixel-antialiased gap-3 ">
-                            <div className="bg-[#5B975D] abolute left-0 top-0 bottom-0 w-1 "></div>
-                            {mesContrats.filter(contrat => {
-                                const day = new Date(contrat.datePrlvt).getDate();
-                                return day <= 15 && contrat.statusAbo === true;
-                            }).map(contrat => (
-                                <li key={contrat.id} className="bg-[#282830] flex w-[90%] relative justify-between p-3  border-l-4 border-l-[#5B975D] rounded-br-[5px] rounded-tr-[5px] drop-figma">
-                                    {contrat.type === "Abonnement" ? (
-                                        <div className="ml-3 flex flex-col justify-between ">
-                                            <strong className="text-lg leading-none">{contrat.nom}</strong>
-                                            <p className="text-sm font-sans font-light">Abonnement actif</p>
-                                        </div>
-                                    ) : (
-                                        <div className="ml-3 flex flex-col justify-between ">
-                                            <strong className="text-lg leading-none">{contrat.nom}</strong>
-                                            <p className="text-sm font-sans font-light">Echéances restantes : <span className="text-sm font-bold">{contrat.echeance}</span></p>
-                                        </div>
-                                    )}
-
-                                    <div className="flex flex-col justify-between leading-none items-end">
-                                        <p className="font-sans text-xs">{new Date(contrat.datePrlvt).toLocaleDateString()}</p>
-                                        <p>{contrat.prix} €</p>
-                                    </div>
-                                </li>
-                            ))}
-                            <div className="total w-[100%] min-w-[390px] bg-[#282830] h-8 text-white flex items-center justify-center drop-figma">
-                                <p className="text-sm font-sans">Total des prélèvements le 15 du mois
-                                    <span className="font-bold text-lg ml-2">
-
-
-
-
-                                        {mesContrats
-                                            .filter((contrat) => new Date(contrat.datePrlvt).getDate() <= 15 && contrat.statusAbo === true)  // Filtrer les contrats
-                                            .reduce((acc, contrat) => acc + (parseFloat(contrat.prix) || 0), 0)  // S'assurer que chaque prix est un nombre valide
-                                            .toFixed(2)} €
-                                    </span>
-                                </p>
-                            </div>
-
-
-
-                        </ul>
-                    )}
-
-
-{mesContrats.some(contrat => {
-                    const day = new Date(contrat.datePrlvt).getDate();
-                    return day >= 15 && contrat.statusAbo === true;
-                }) && (
-                        <ul className="flex justify-center w-[100%] flex-col items-center relative mt-3 subpixel-antialiased gap-3 ">
-                            <div className="bg-[#5B975D] abolute left-0 top-0 bottom-0 w-1 "></div>
-                            {mesContrats.filter(contrat => {
-                                const day = new Date(contrat.datePrlvt).getDate();
-                                return day >= 15 && contrat.statusAbo === true;
-                            }).map(contrat => (
-                                <li key={contrat.id} className="bg-[#282830] flex w-[90%] relative justify-between p-3  border-l-4 border-l-[#5B975D] rounded-br-[5px] rounded-tr-[5px] drop-figma">
-                                    {contrat.type === "Abonnement" ? (
-                                        <div className="ml-3 flex flex-col justify-between ">
-                                            <strong className="text-lg leading-none">{contrat.nom}</strong>
-                                            <p className="text-sm font-sans font-light">Abonnement actif</p>
-                                        </div>
-                                    ) : (
-                                        <div className="ml-3 flex flex-col justify-between ">
-                                            <strong className="text-lg leading-none">{contrat.nom}</strong>
-                                            <p className="text-sm font-sans font-light">Echéances restantes : <span className="text-sm font-bold">{contrat.echeance}</span></p>
-                                        </div>
-                                    )}
-
-                                    <div className="flex flex-col justify-between leading-none items-end">
-                                        <p className="font-sans text-xs">{new Date(contrat.datePrlvt).toLocaleDateString()}</p>
-                                        <p>{contrat.prix} €</p>
-                                    </div>
-                                </li>
-                            ))}
-                            <div className="total w-[100%] min-w-[390px] bg-[#282830] h-8 text-white flex items-center justify-center drop-figma">
-                                <p className="text-sm font-sans">Total des prélèvements le 30 du mois
-                                    <span className="font-bold text-lg ml-2">
-
-
-
-
-                                        {mesContrats
-                                            .filter((contrat) => new Date(contrat.datePrlvt).getDate() >= 15 && contrat.statusAbo === true)  // Filtrer les contrats
-                                            .reduce((acc, contrat) => acc + (parseFloat(contrat.prix) || 0), 0)  // S'assurer que chaque prix est un nombre valide
-                                            .toFixed(2)} €
-                                    </span>
-                                </p>
-                            </div>
-
-
-
-                        </ul>
-                    )}
-
+                <ListeContratsInf15 />
+                <ListeContratsSup15 />
                 
             </div>
             {scrollDown &&
